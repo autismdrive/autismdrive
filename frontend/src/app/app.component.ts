2ac4b0c5e4fff7dd5932a25426f153fd42242d46
eleanor <eleanorcjkgraham@gmail.com>
2018-11-26 14:46:10 -0500

    add hide header for routes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, ActivationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'star-drive';
+  hideHeader = false;
+
+  public constructor (
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+
+    this.router.events.subscribe((e) => {
+      if (e instanceof ActivationStart || e instanceof ActivationEnd) {
+        if (e.snapshot && e.snapshot.data) {
+          const data = e.snapshot.data;
+          this.hideHeader = !!data.hideHeader;
+        }
+      }
+    });
+  }
 }
