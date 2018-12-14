16ec0131058fd594bbeafb1369368c3d38f9441d
eleanor <eleanorcjkgraham@gmail.com>
2018-12-14 10:36:34 -0500

    change user model for login

--- backend/app/model/user.py
+++ backend/app/model/user.py
@@ -1,17 +1,19 @@
 import datetime
+import jwt
 from sqlalchemy.ext.hybrid import hybrid_property
 
-from app import db, RestException, bcrypt
+from app import app, db, RestException, bcrypt
 
 
 class User(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 'stardrive_user'
     id = db.Column(db.Integer, primary_key=True)
     last_updated = db.Column(db.DateTime, default=datetime.datetime.now)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
     email = db.Column(db.String, nullable=False, unique=True)
     role = db.Column(db.String, default='Self')
+    email_verified = db.Column(db.Boolean, nullable=False, default=False)
     _password = db.Column('password', db.Binary(60))
 
 
@@ -27,3 +29,30 @@ class User(db.Model):
         if not self._password:
             raise RestException(RestException.LOGIN_FAILURE)
         return bcrypt.check_password_hash(self._password, plaintext)
+
+    def encode_auth_token(self):
+        try:
+            payload = {
+                'exp':
+                    datetime.datetime.utcnow() + datetime.timedelta(
+                        hours=2, minutes=0, seconds=0),
+                'iat':
+                    datetime.datetime.utcnow(),
+                'sub':
+                    self.id
+            }
+            return jwt.encode(
+                payload, app.config.get('SECRET_KEY'), algorithm='HS256')
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        try:
+            payload = jwt.decode(
+                auth_token, app.config.get('SECRET_KEY'), algorithms='HS256')
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            raise RestException(RestException.TOKEN_EXPIRED)
+        except jwt.InvalidTokenError:
+            raise RestException(RestException.TOKEN_INVALID)
