97058f8a41925f43974c4dcb829aae96f163ea5d
Aaron Louie <ajlouie@ux-ninja.com>
2019-01-31 14:07:59 -0500

    Generates repeating form sections recursively.

--- frontend/src/app/enrollment-flow/enrollment-flow.component.ts
+++ frontend/src/app/enrollment-flow/enrollment-flow.component.ts
@@ -19,7 +19,13 @@ export class EnrollmentFlowComponent implements OnInit {
   stepName: string;
   activeStep = 0;
   loading = true;
-  stepNames = ['identification', 'contact', 'demographics', 'home', 'evaluation_history'];
+  stepNames = [
+    'identification',
+    'contact',
+    'demographics',
+    'home',
+    'evaluation_history'
+  ];
 
   model = {};
   step: QuestionnaireStep;
@@ -38,7 +44,7 @@ export class EnrollmentFlowComponent implements OnInit {
 
         if (stepName !== '') {
           this.api.getQuestionnaireMeta(stepName).subscribe(q => {
-            this.step = this.infoToFormlyForm(q.get_meta, stepName);
+            this.step = this._infoToFormlyForm(q.get_meta, stepName);
             this.form = new FormArray([new FormGroup({})]);
             this.options = <FormlyFormOptions>{};
             this.loading = false;
@@ -56,13 +62,14 @@ export class EnrollmentFlowComponent implements OnInit {
   ngOnInit() {
   }
 
-  infoToFormlyForm(info, stepName): QuestionnaireStep {
+  private _infoToFormlyForm(info, stepName, fieldsType = 'fields'): QuestionnaireStep {
     const step = new QuestionnaireStep({
       id: info.id,
       name: stepName,
       label: '',
       description: '',
-      fields: []
+      fields: [],
+      fieldGroup: [],
     });
     const stepFields = [];
 
@@ -80,21 +87,20 @@ export class EnrollmentFlowComponent implements OnInit {
           const wrapper = this.clone(item[wrapperKey]);
           const fgFields = item[wrapperKey].fields || [];
           wrapper.key = wrapperKey;
-          wrapper.fieldGroup = fgFields.map((childKey: string) => {
-            const childField = this.clone(info[childKey]);
-            childField.key = childKey;
-            childField.name = childKey;
 
-            // Remove the field from the 'all' object, since we've
-            // now copied it to its parent fieldGroup
-            delete info[childKey];
-            return keysToCamel(childField);
-          });
+          if (wrapper.type === 'repeat') {
+            // Recursively process sub-forms and insert them into fieldArray.
+            // Fields will be inserted into 'fieldGroup', rather than 'fields' attribute.
+            wrapper.fieldArray = this._infoToFormlyForm(info[wrapperKey], wrapperKey, 'fieldGroup');
+          } else {
+            wrapper.fieldGroup = this._mapFieldnamesToFieldGroup(fgFields, info);
 
-          // Remove the fields array from the wrapper object,
-          // since all its child fields are now inside the
-          // fieldGroup attribute
-          delete wrapper.fields;
+            // Remove the fields array from the wrapper object,
+            // since all its child fields are now inside the
+            // fieldGroup attribute
+            delete wrapper.fields;
+          }
+
           stepFields.push(keysToCamel(wrapper));
         });
       } else if (item) {
@@ -105,7 +111,7 @@ export class EnrollmentFlowComponent implements OnInit {
     });
 
     stepFields.sort((f1, f2) => f1.displayOrder - f2.displayOrder);
-    stepFields.forEach(f => step.fields.push(f));
+    stepFields.forEach(f => step[fieldsType].push(f));
     return step;
   }
 
@@ -139,18 +145,31 @@ export class EnrollmentFlowComponent implements OnInit {
     if (isFinite(this.step.id)) {
       this.api.updateQuestionnaire(this.step.name, this.step.id, options).subscribe(response => {
         // Update form with saved values
-        this.router.navigate(['profile', this.stepNames[this.activeStep]]);
+        this.router.navigate(['profile', 'enrollment', this.stepNames[this.activeStep]]);
       });
     } else {
       this.api.submitQuestionnaire(this.step.name, options).subscribe(response => {
         // Update form with saved values
-        this.router.navigate(['profile', this.stepNames[this.activeStep]]);
+        this.router.navigate(['profile', 'enrollment', this.stepNames[this.activeStep]]);
       });
     }
   }
 
   clone(o: any): any {
     return JSON.parse(JSON.stringify(o));
+  }
+
+  private _mapFieldnamesToFieldGroup(fieldnames: string[], parentObject) {
+    return fieldnames.map((childKey: string) => {
+      const childField = this.clone(parentObject[childKey]);
+      childField.key = childKey;
+      childField.name = childKey;
+
+      // Remove the field from the 'all' object, since we've
+      // now copied it to its parent fieldGroup
+      delete parentObject[childKey];
+      return keysToCamel(childField);
+    });
   }
 
 }
