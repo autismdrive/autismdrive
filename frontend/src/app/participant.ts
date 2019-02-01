29b64be5c812171dd52340d4097676fb8b6daa25
Aaron Louie <ajlouie@ux-ninja.com>
2019-02-01 16:16:59 -0500

    Adds Participant and UserParticipant

--- frontend/src/app/participant.ts
+++ frontend/src/app/participant.ts
@@ -1,11 +1,18 @@
-export interface Participant {
+import { User } from './user';
 
+export interface Participant {
+  id: number;
+  last_updated: Date;
   first_name: string;
-  nickname: string;
   last_name: string;
-  relationship: string;
-  percent_complete: number;
-  num_studies_enrolled: number;
-  avatar_img_url: string;
+  users: User[];
+
+  // first_name: string;
+  // nickname: string;
+  // last_name: string;
+  // relationship: string;
+  // percent_complete: number;
+  // num_studies_enrolled: number;
+  // avatar_img_url: string;
 
 }
--- frontend/src/app/user.ts
+++ frontend/src/app/user.ts
@@ -1,7 +1,10 @@
+import { UserParticipant } from './user-participant';
+
 export interface User {
   id: number;
   first_name: string;
   last_name: string;
   email: string;
   role: string;
+  participants: UserParticipant[];
 }
