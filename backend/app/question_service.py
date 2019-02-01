f424d934071e23867151ae86668636e630a629f8
Dan Funk <daniel.h.funk@gmail.com>
2019-02-01 14:58:18 -0500

    Adding two transient (not saved to the db) fields to all questions:  __question_type__ (identifying,sensitive,unrestricted) and __estimated_duration_minutes__ (time we expect it to take to complete a question in minutes). Adding one persistent field to all questionnaires - a "time_on_task_ms" to record how long people actually spend completeing a questionnaire. Because many endpoints and models need to talk about the same constants and access some general methods around working with Questionnaires, I've created a service that houses this logic in a single place.

---
+++ backend/app/question_service.py
@@ -0,0 +1,50 @@
+import importlib
+
+
+class QuestionService:
+
+    QUESTION_PACKAGE = "app.model.questionnaires"
+
+    TYPE_SENSITIVE = 'sensitive'
+    TYPE_IDENTIFYING = 'identifying'
+    TYPE_UNRESTRICTED = 'unrestricted'
+
+    @staticmethod
+    def get_class(name):
+        module_name = QuestionService.QUESTION_PACKAGE + "." + name;
+        class_name = QuestionService.camel_case_it(name)
+        return QuestionService.str_to_class(module_name, class_name)
+
+    @staticmethod
+    def get_schema(name, many=False):
+        module_name = QuestionService.QUESTION_PACKAGE + "." + name
+        schema_name = QuestionService.camel_case_it(name) + "Schema"
+        return QuestionService.str_to_class(module_name, schema_name)(many=many)
+
+    @staticmethod
+    def get_meta_schema(name):
+        module_name = QuestionService.QUESTION_PACKAGE + "." + name
+        schema_name = QuestionService.camel_case_it(name) + "MetaSchema"
+        return QuestionService.str_to_class(module_name, schema_name)()
+
+    @staticmethod
+    def camel_case_it(name):
+        first, *rest = name.split('_')
+        return first.capitalize() + ''.join(word.capitalize() for word in rest)
+
+    # Given a string, creates an instance of that class
+    @staticmethod
+    def str_to_class(module_name, class_name):
+        try:
+            module_ = importlib.import_module(module_name)
+            try:
+                return getattr(module_, class_name)
+            except AttributeError:
+                # FIXME: Get some damn logging.
+                print("Error class does not exist:" + class_name)
+                # logging.ERROR('Class does not exist')
+        except ImportError:
+            # FIXME: Get some damn logging.
+            print("Module does not exist:" + module_name)
+            #   logging.('Module does not exist')
+        return None
