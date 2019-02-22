884b6409600d82468f9736992ccea1a6ccc1e39e
eleanor <eleanorcjkgraham@gmail.com>
2019-02-22 13:33:45 -0500

    add migration to merge two heads

---
+++ backend/migrations/versions/29012e868555_merge_5c8481ca872f_and_453728f0baa6.py
@@ -0,0 +1,24 @@
+"""merge 5c8481ca872f and 453728f0baa6
+
+Revision ID: 29012e868555
+Revises: 5c8481ca872f, 453728f0baa6
+Create Date: 2019-02-22 13:32:38.565008
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '29012e868555'
+down_revision = ('5c8481ca872f', '453728f0baa6')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
