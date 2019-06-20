7897928849fe5c8fbdeb422e2ae43b05175904fe
eleanor <eleanorcjkgraham@gmail.com>
2019-06-20 14:42:43 -0400

    set display for last-updated dates in user admin detail; set percentage complete for participants

--- frontend/src/app/user-admin-details/user-admin-details.component.ts
+++ frontend/src/app/user-admin-details/user-admin-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Participant } from '../_models/participant';
 import { User } from '../_models/user';
-import {ApiService} from '../_services/api/api.service';
+import { ApiService } from '../_services/api/api.service';
 
 @Component({
   selector: 'app-user-admin-details',
@@ -11,13 +12,23 @@ import {ApiService} from '../_services/api/api.service';
 export class UserAdminDetailsComponent implements OnInit {
   user: User;
 
-  constructor(private api: ApiService, private route: ActivatedRoute) {
+  constructor(
+    private api: ApiService, private route: ActivatedRoute
+  ) {
     this.route.params.subscribe(params => {
       const userId = params.userId ? parseInt(params.userId, 10) : null;
 
       if (isFinite(userId)) {
         this.api.getUser(userId).subscribe(user => {
           this.user = user;
+          for (let pi in this.user.participants) {
+              let participant = new Participant(this.user.participants[pi]);
+              this.api.getFlow(participant.getFlowName(), participant.id)
+              .subscribe(f => {
+                participant.percent_complete = f.percentComplete();
+                this.user.participants[pi] = participant;
+              })
+          }
         });
       }
     });
