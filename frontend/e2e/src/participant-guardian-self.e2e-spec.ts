48afde86ee2b02fb82c726b6a9f9e15e41efb66c
Aaron Louie <aaron@sartography.com>
2019-08-16 17:35:03 -0400

    WIP: Fixing broken tests

--- frontend/e2e/src/participant-guardian-self.e2e-spec.ts
+++ frontend/e2e/src/participant-guardian-self.e2e-spec.ts
@@ -69,12 +69,12 @@ describe('Participant (Guardian - Self)', () => {
 
   // Enrollment Flow
   it('should display the terms of consent to the study', () => enrollUseCases.displayGuardianTerms());
-  it('should cancel out of the terms consent page', () => enrollUseCases.cancelEditing());
+  it('should cancel out of the terms consent page', () => enrollUseCases.cancelTerms());
   it('should navigate back to the Guardian flow', () => profileUseCases.navigateToGuardianFlow());
   it('should accept the terms', () => enrollUseCases.acceptTerms());
   it('should display a menu link to all steps of the flow', () => enrollUseCases.displayMenuLinks());
   it('should display completed status of each step', () => enrollUseCases.displayCompletedStatus());
-  it('should cancel editing enrollment info', () => enrollUseCases.cancelEditing());
+  it('should cancel enrollment instructions', () => enrollUseCases.cancelIntro());
   it('should navigate back to the Guardian flow', () => profileUseCases.navigateToGuardianFlow());
   it('should navigate to each step of the flow', () => enrollUseCases.navigateToEachStep());
   it('should cancel editing enrollment info', () => enrollUseCases.cancelEditing());
