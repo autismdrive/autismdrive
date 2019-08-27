fe55c9549f82678d9fdb4b6e0074fb3c00d5f932
Aaron Louie <aaron@sartography.com>
2019-08-27 16:47:35 -0400

    Redesigns search page and makes it mostly functional. Still quite a bit of tweaking to do.

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -1,30 +1,31 @@
-import { LatLngLiteral } from '@agm/core';
-import { MediaMatcher } from '@angular/cdk/layout';
-import {
-  AfterViewInit,
-  ChangeDetectorRef,
-  Component,
-  OnDestroy,
-  OnInit,
-  Renderer2,
-  ViewChild
-} from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSidenav } from '@angular/material/sidenav';
-import { ActivatedRoute, Params, Router } from '@angular/router';
-import { Filter, Query, Sort, Hit, HitLabel } from '../_models/query';
-import { SearchService } from '../_services/api/search.service';
-import { StudyStatus } from '../_models/study';
+import {LatLngLiteral} from '@agm/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {Filter, HitLabel, HitType, Query, Sort} from '../_models/query';
+import {SearchService} from '../_services/api/search.service';
 import {merge} from 'rxjs';
 import {tap} from 'rxjs/operators';
 import {GoogleAnalyticsService} from '../google-analytics.service';
 import {scrollToTop} from '../../util/scrollToTop';
-import {log} from "util";
+import {User} from '../_models/user';
+import {AuthenticationService} from '../_services/api/authentication-service';
+import {AccordionItem} from '../_models/accordion-item';
 
 interface SortMethod {
   name: string;
   label: string;
   sortQuery: Sort;
+}
+
+interface ResourceType {
+  name: string;
+  label: string;
+}
+
+class MapControlDiv extends HTMLDivElement {
+  index?: number;
 }
 
 @Component({
@@ -33,22 +34,27 @@ interface SortMethod {
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
+  resourceTypes: ResourceType[] = ['RESOURCE', 'LOCATION', 'EVENT'].map(t => {
+    return {name: HitType[t], label: HitLabel[t]};
+  });
   query = new Query({
     words: '',
     filters: [],
     size: 20,
   });
-  showFilters = false;
+  locQuery: Query;
   loading = true;
   hideResults = false;
   filters: Filter[];
   pageSize = 20;
-  mapLoc: LatLngLiteral = {
+  mapLoc: LatLngLiteral;
+
+  defaultLoc: LatLngLiteral = {
     lat: 37.9864031,
     lng: -81.6645856
   };
+
   mobileQuery: MediaQueryList;
-  private _mobileQueryListener: () => void;
   sortMethods: SortMethod[] = [
     {
       name: 'Relevance',
@@ -63,8 +69,8 @@ export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
       label: 'Near me',
       sortQuery: {
         field: 'geo_point',
-        latitude: this.mapLoc.lat,
-        longitude: this.mapLoc.lng,
+        latitude: this.mapLoc ? this.mapLoc.lat : this.defaultLoc.lat,
+        longitude: this.mapLoc ? this.mapLoc.lng : this.defaultLoc.lng,
         order: 'asc',
         unit: 'mi'
       }
@@ -79,9 +85,43 @@ export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
     },
   ];
   selectedSort: SortMethod;
-  @ViewChild('sidenav', {static: true}) public sideNav: MatSidenav;
   @ViewChild('paginator', {static: true}) paginator: MatPaginator;
-  featuredStudies: Hit[];
+  currentUser: User;
+  resourceGatherers: AccordionItem[] = [
+    {
+      name: 'Charlottesville Region Autism Action Group',
+      shortName: 'CRAAG',
+      description: `
+        CRAAG is a parent-run advocacy group, one of three active all-volunteer regional Autism Action Groups
+        initiated by Commonwealth Autism. Established in 2010, it serves Charlottesville, Albemarle, Greene,
+        Fluvanna, Louisa, and Nelson counties.
+      `,
+      image: '/assets/partners/craag.png',
+      url: 'https://cahumanservices.org/advocating-change/community-organization-engagement/autism-action-groups/',
+    },
+    {
+      name: 'UVA Supporting Transformative Autism Research',
+      shortName: 'UVA STAR',
+      description: `
+        The STAR initiative, led by the Curry School in partnership with colleagues across the University,
+        aims to improve the lives of individuals with autism through groundbreaking research and innovative
+        models for intervention and training.
+      `,
+      image: '/assets/partners/uva_star.png',
+      url: 'https://curry.virginia.edu/faculty-research/centers-labs-projects/supporting-transformative-autism-research-star',
+    },
+    {
+      name: 'Autism Speaks Inc.',
+      shortName: 'Autism Speaks',
+      description: `
+        Autism Speaks Inc. is the largest autism advocacy organization in the United States. It sponsors autism research
+        and conducts awareness and outreach activities aimed at families, governments, and the public.
+      `,
+      image: '/assets/partners/autism_speaks.png',
+      url: 'https://www.autismspeaks.org/',
+    }
+  ];
+  private readonly _mobileQueryListener: () => void;
 
   constructor(
     changeDetectorRef: ChangeDetectorRef,
@@ -89,14 +129,20 @@ export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
     private router: Router,
     private renderer: Renderer2,
     private searchService: SearchService,
+    private locSearchService: SearchService,
     private featuredSearchService: SearchService,
     private googleAnalyticsService: GoogleAnalyticsService,
+    private authenticationService: AuthenticationService,
     media: MediaMatcher,
   ) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
     this.mobileQuery = media.matchMedia('(max-width: 959px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+
+
     this.loadMapLocation(() => {
+      this.loadLocResources();
       this.route.queryParamMap.subscribe(qParams => {
         let words = '';
         const filters: Filter[] = [];
@@ -105,7 +151,7 @@ export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
           if (key === 'words') {
             words = qParams.get(key);
           } else {
-            filters.push({ field: key, value: qParams.getAll(key) });
+            filters.push({field: key, value: qParams.getAll(key)});
           }
         }
 
@@ -116,12 +162,7 @@ export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
         });
 
         this.sortBy(this.sortMethods[0]);
-        this.updateFilters();
       });
-    });
-
-    this.renderer.listen(window, 'resize', (event) => {
-      this.checkWindowWidth();
     });
   }
 
@@ -138,14 +179,32 @@ export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
     this.searchService.reset();
   }
 
-  private checkWindowWidth(): void {
-    if (window.innerWidth > 768) {
-      this.sideNav.mode = 'side';
-    } else {
-      this.sideNav.mode = 'over';
+  loadLocResources() {
+    this.loading = true;
+
+    this.locQuery = new Query({
+      filters: [{field: 'Type', value: HitLabel.LOCATION}],
+      start: 0,
+      size: 999
+    });
+    if (this.mapLoc) {
+      this.locQuery.sort = {
+        field: 'geo_point',
+        latitude: this.mapLoc.lat,
+        longitude: this.mapLoc.lng,
+        order: 'asc',
+        unit: 'mi'
+      };
     }
 
-    this.sideNav.opened = this.showFilters && !this.mobileQuery.matches;
+    this.locSearchService
+      .search(this.locQuery)
+      .subscribe(queryWithResults => {
+        if (this.locQuery.equals(queryWithResults)) {
+          this.locQuery = queryWithResults;
+          this.loading = false;
+        }
+      });
   }
 
   removeWords() {
@@ -173,11 +232,11 @@ export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
     }
 
     this.router.navigate(
-    [],
-    {
-      relativeTo: this.route,
-      queryParams: queryParams
-    }).then(() => {
+      [],
+      {
+        relativeTo: this.route,
+        queryParams: queryParams
+      }).then(() => {
       console.log('Done navigating to new route.', queryParams);
       scrollToTop();
     });
@@ -190,7 +249,6 @@ export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
       .subscribe(queryWithResults => {
         if (this.query.equals(queryWithResults)) {
           this.query = queryWithResults;
-          this.checkWindowWidth();
           this.loading = false;
         } else {
           console.log('queryWithResults', queryWithResults);
@@ -223,7 +281,6 @@ export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
     this.query.start = 0;
     this.query.sort = selectedSort.sortQuery;
 
-
     if (selectedSort.name === 'Distance') {
       this.loadMapLocation(() => {
         this.query.sort.latitude = this.mapLoc.lat;
@@ -252,6 +309,19 @@ export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
     this.updateUrl(this.query);
   }
 
+  selectResourceType(keepType: string) {
+    this.resourceTypes.forEach(t => {
+      if (t.label === keepType) {
+        this.query.addFilter('Type', t.label);
+      } else {
+        this.query.removeFilter('Type', t.label);
+      }
+    });
+
+    this.query.start = 0;
+    this.updateUrl(this.query);
+  }
+
   updatePage() {
     if (this.paginator) {
       this.query.size = this.paginator.pageSize;
@@ -260,11 +330,66 @@ export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
     }
   }
 
-  // Show filters if all filters have been removed.
-  updateFilters() {
-    this.showFilters = this.query.filters.length === 0;
-    if (!this.mobileQuery.matches && this.showFilters && this.sideNav) {
-      this.sideNav.open();
+  addMyLocationControl(mapUI: google.maps.Map) {
+    const controlDiv: MapControlDiv = document.createElement('div');
+
+    // Set CSS for the control border.
+    const controlUI = document.createElement('div');
+    controlUI.style.backgroundColor = '#fff';
+    controlUI.style.border = '2px solid #fff';
+    controlUI.style.borderRadius = '3px';
+    controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.marginBottom = '6px';
+    controlUI.style.marginRight = '12px';
+    controlUI.style.textAlign = 'center';
+    controlUI.title = 'Your Location';
+    controlDiv.appendChild(controlUI);
+
+    // Set CSS for the control interior.
+    const controlText = document.createElement('div');
+    controlText.style.fontSize = '16px';
+    controlText.style.lineHeight = '38px';
+    controlText.style.paddingLeft = '5px';
+    controlText.style.paddingRight = '5px';
+    controlText.innerHTML = '<img src="/assets/map/my-location.svg">';
+    controlUI.appendChild(controlText);
+
+    // Set the center to the user's location on click
+    controlUI.addEventListener('click', () => {
+      this.loadMapLocation(() => {
+        mapUI.setCenter(this.mapLoc);
+        mapUI.setZoom(9);
+      });
+    });
+
+    controlDiv.index = 1;
+    mapUI.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(controlDiv);
+  }
+
+  addMapResources( mapUI: google.maps.Map) {
+    if (this.locQuery) {
+      console.log(this.locQuery.hits);
     }
+  }
+
+  protected mapLoad(map: google.maps.Map) {
+    this.addMyLocationControl(map);
+    this.addMapResources(map);
+  }
+
+  isSelectedResourceType(rt: ResourceType) {
+    const types = this.query.filters.find(f => f.field === 'Type');
+
+    if (types) {
+      const f = types.value.find(t => {
+        return t === rt.label;
+      });
+
+      if (f) {
+        return true;
+      }
+    }
+    return false;
   }
 }
