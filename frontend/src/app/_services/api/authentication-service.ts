f1056b16becdfed14834cfd62e2e8a1416040e6f
Dan Funk <daniel.h.funk@gmail.com>
2019-08-28 12:15:22 -0400

    Fixes the new session timeout issue that was preventing users from logging in.

--- frontend/src/app/_services/api/authentication-service.ts
+++ frontend/src/app/_services/api/authentication-service.ts
@@ -11,7 +11,6 @@ import {GoogleAnalyticsService} from '../../google-analytics.service';
 export class AuthenticationService {
   public static LOCAL_TOKEN_KEY = 'star_token';
 
-  private token: String;
   private currentUserSubject: BehaviorSubject<User>;
   public currentUser: Observable<User>;
   login_url = `${environment.api}/api/login_password`;
@@ -20,10 +19,10 @@ export class AuthenticationService {
 
 
   constructor(private http: HttpClient, private googleAnalyticsService: GoogleAnalyticsService) {
-    this.token = localStorage.getItem(AuthenticationService.LOCAL_TOKEN_KEY);
+    const token = localStorage.getItem(AuthenticationService.LOCAL_TOKEN_KEY);
     this.currentUserSubject = new BehaviorSubject<User>(null);
     this.currentUser = this.currentUserSubject.asObservable();
-    if (this.token) {
+    if (token) {
       this._refresh().subscribe();  // Make sure the api still considers the in-memory user as valid.
     }
   }
@@ -35,14 +34,13 @@ export class AuthenticationService {
 
   private loadUser(userDict): User {
     // login successful if there's a jwt token in the response
-    if (userDict && userDict.token) {
-      const user = new User(userDict);
-      // store user details and jwt token in local storage to keep user logged in between page refreshes
-      localStorage.setItem(AuthenticationService.LOCAL_TOKEN_KEY, user.token);
-      this.currentUserSubject.next(user);
-      this.googleAnalyticsService.set_user(user.id);
-      return user;
+    if (userDict.token) {
+      localStorage.setItem(AuthenticationService.LOCAL_TOKEN_KEY, userDict.token);
     }
+    const user = new User(userDict);
+    this.currentUserSubject.next(user);
+    this.googleAnalyticsService.set_user(user.id);
+    return user;
   }
 
   login(email: string, password: string, email_token = ''): Observable<User>  {
@@ -57,7 +55,6 @@ export class AuthenticationService {
     // For a refresh, we'll hold to the existing token, and try to make the request.
     return this.http.get<any>(this.refresh_url)
       .pipe(map(userDict => {
-        userDict.token = this.token;
         return this.loadUser(userDict);
       }),
       );
