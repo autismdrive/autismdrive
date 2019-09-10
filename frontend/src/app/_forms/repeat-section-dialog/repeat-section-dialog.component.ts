b046410445935538c76db2a2e959c95a2be7d747
Aaron Louie <aaron@sartography.com>
2019-09-10 16:42:31 -0400

    Disables save button if form is not valid.

--- frontend/src/app/_forms/repeat-section-dialog/repeat-section-dialog.component.ts
+++ frontend/src/app/_forms/repeat-section-dialog/repeat-section-dialog.component.ts
@@ -1,7 +1,5 @@
-import {AfterViewInit, Component, Inject, OnInit} from '@angular/core';
+import {AfterContentChecked, Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
-import {FormlyFieldConfig, FormlyFormOptions} from '@ngx-formly/core';
-import {FormGroup} from '@angular/forms';
 import {RepeatSectionDialogData} from '../../_models/repeat_section_dialog_data';
 
 @Component({
@@ -9,12 +7,27 @@ import {RepeatSectionDialogData} from '../../_models/repeat_section_dialog_data'
   templateUrl: './repeat-section-dialog.component.html',
   styleUrls: ['./repeat-section-dialog.component.scss']
 })
-export class RepeatSectionDialogComponent {
+export class RepeatSectionDialogComponent implements AfterContentChecked {
+  disableSave: boolean;
+
   constructor(
     public dialogRef: MatDialogRef<RepeatSectionDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: RepeatSectionDialogData
   ) {
-    console.log('RepeatSectionDialogComponent constructor this.data', this.data);
+  }
+
+  ngAfterContentChecked(): void {
+    this.updateDisableSave();
+  }
+
+  updateDisableSave() {
+    this.disableSave = !this.noErrors();
+  }
+
+  noErrors(): boolean {
+    return this.data.fields.every(f => {
+      return f.formControl && f.formControl.valid;
+    });
   }
 
   onNoClick(): void {
@@ -22,6 +35,8 @@ export class RepeatSectionDialogComponent {
   }
 
   onSubmit(): void {
-    this.dialogRef.close(this.data);
+    if (this.noErrors()) {
+      this.dialogRef.close(this.data);
+    }
   }
 }
