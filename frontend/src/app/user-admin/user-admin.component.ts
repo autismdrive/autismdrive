8009ca7bdd64cb835b44f5a7ff504b976889ad49
eleanor <eleanorcjkgraham@gmail.com>
2019-11-04 09:24:41 -0500

    WIP: add last_updated date column to user admin - still need to fix sorting in that column

--- frontend/src/app/user-admin/user-admin.component.ts
+++ frontend/src/app/user-admin/user-admin.component.ts
@@ -17,7 +17,7 @@ import { Router } from '@angular/router';
 export class UserAdminComponent implements OnInit, AfterViewInit {
 
   dataSource: UserDataSource;
-  displayedColumns = ['id', 'role', 'email', 'participants'];
+  displayedColumns = ['id', 'role', 'last_updated', 'email', 'participants'];
   default_page_size = 10;
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
