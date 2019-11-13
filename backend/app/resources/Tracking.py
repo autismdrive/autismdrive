180e019f580ed31d4761fa03247e261b04d1c4d9
eleanor <eleanorcjkgraham@gmail.com>
2019-11-13 12:12:49 -0500

    update STAR logo for HTML email template

--- backend/app/resources/Tracking.py
+++ backend/app/resources/Tracking.py
@@ -9,7 +9,7 @@ from flask import Blueprint, send_file
 tracking_blueprint = Blueprint('track', __name__, url_prefix='/api/track')
 
 
-@tracking_blueprint.route('/<string:user_id>/<string:code>/logo.png')
+@tracking_blueprint.route('/<string:user_id>/<string:code>/UVA_STAR-logo.png')
 def logo(user_id, code):
     email_log = EmailLog.query.filter_by(user_id=user_id, tracking_code=code).first()
     if email_log:
@@ -17,4 +17,4 @@ def logo(user_id, code):
         email_log.date_viewed = datetime.datetime.now()
         db.session.add(email_log)
         db.session.commit()
-    return send_file("static/logo.png", mimetype='image/png')
+    return send_file("static/UVA_STAR-logo.png", mimetype='image/png')
