6acbf49c7930f5c083086b7e2876d5e9331afb6e
eleanor <eleanorcjkgraham@gmail.com>
2020-03-13 10:18:07 -0400

    refactor prompting emails to consolidate shared code between the types; still need correct logic for the registration and dependent profile prompts

--- backend/app/prompting_emails.py
+++ backend/app/prompting_emails.py
@@ -11,52 +11,43 @@ from app.email_service import EmailService
 
 class PromptingEmails:
 
-    def send_set_password_prompts(self):
-        recipients = db.session.query(User).filter_by(password=None).all()
+    def send_prompts(self, recipients, send_method, log_type):
         for rec in recipients:
             confirm_logs = db.session.query(EmailLog)\
                 .filter_by(user_id=rec.id)\
-                .filter_by(type='confirm_email') \
+                .filter_by(type=log_type) \
                 .order_by(EmailLog.last_updated).all()
             if len(confirm_logs) <= 2:
                 most_recent = confirm_logs[-1]
                 if (datetime.datetime.now(tz=UTC) - most_recent.last_updated).total_seconds() > 604800:
-                    self.send_confirm_email(rec)
+                    self.send_prompting_email(rec, send_method, log_type)
             elif len(confirm_logs) is 3:
                 most_recent = confirm_logs[-1]
                 if (datetime.datetime.now(tz=UTC) - most_recent.last_updated).total_seconds() > 1314900:
-                    self.send_confirm_email(rec)
+                    self.send_prompting_email(rec, send_method, log_type)
             elif 3 < len(confirm_logs) < 6:
                 most_recent = confirm_logs[-1]
                 if (datetime.datetime.now(tz=UTC) - most_recent.last_updated).total_seconds() > 2629800:
-                    self.send_confirm_email(rec)
+                    self.send_prompting_email(rec, send_method, log_type)
 
     @staticmethod
-    def send_confirm_email(user):
+    def send_prompting_email(user, send_method, log_type):
         current_studies = db.session.query(Study).filter_by(status='currently_enrolling').all()
         for study in current_studies:
             study.link = app.config['SITE_URL'] + '/#/study/' + str(study.id)
-        tracking_code = EmailService(app).confirm_email(user, current_studies)
-        log = EmailLog(user_id=user.id, type="confirm_email", tracking_code=tracking_code)
+        tracking_code = send_method(user, current_studies)
+        log = EmailLog(user_id=user.id, type=log_type, tracking_code=tracking_code)
         db.session.add(log)
         db.session.commit()
 
-    @staticmethod
-    def send_complete_registration_email(user):
-        current_studies = db.session.query(Study).filter_by(status='currently_enrolling').all()
-        for study in current_studies:
-            study.link = app.config['SITE_URL'] + '/#/study/' + str(study.id)
-        tracking_code = EmailService(app).complete_registration_prompt_email(user, current_studies)
-        log = EmailLog(user_id=user.id, type="complete_registration_prompt", tracking_code=tracking_code)
-        db.session.add(log)
-        db.session.commit()
+    def send_confirm_prompting_emails(self):
+        recipients = db.session.query(User).filter_by(password=None).all()
+        self.send_prompts(recipients, EmailService(app).confirm_email, 'confirm_email')
 
-    @staticmethod
-    def send_dependent_profile_email(user):
-        current_studies = db.session.query(Study).filter_by(status='currently_enrolling').all()
-        for study in current_studies:
-            study.link = app.config['SITE_URL'] + '/#/study/' + str(study.id)
-        tracking_code = EmailService(app).complete_dependent_profile_prompt_email(user, current_studies)
-        log = EmailLog(user_id=user.id, type="dependent_profile_prompt", tracking_code=tracking_code)
-        db.session.add(log)
-        db.session.commit()
+    def send_complete_registration_prompting_emails(self):
+        recipients = db.session.query(User).filter_by(password=None).all()
+        self.send_prompts(recipients, EmailService(app).complete_registration_prompt_email, 'complete_registration_prompt')
+
+    def send_dependent_profile_prompting_emails(self):
+        recipients = db.session.query(User).filter_by(password=None).all()
+        self.send_prompts(recipients, EmailService(app).complete_dependent_profile_prompt_email, 'dependent_profile_prompt')
--- backend/tests/test_email_automation.py
+++ backend/tests/test_email_automation.py
@@ -14,103 +14,160 @@ from app.prompting_emails import PromptingEmails
 
 class TestExportCase(BaseTestQuestionnaire, unittest.TestCase):
 
-    def create_email_log_records(self, num_records, days_removed):
+    def create_email_log_records(self, num_records, days_removed, log_type):
         for _ in range(num_records):
             log = EmailLog(last_updated=datetime.datetime.now() - datetime.timedelta(days=days_removed),
-                           user_id=self.construct_user().id, type="confirm_email")
+                           user_id=self.construct_user().id, type=log_type)
             db.session.add(log)
             db.session.commit()
             days_removed += 2
 
-    def test_confirm_email_followup_sent_after_7_days(self):
+    def test_prompting_emails_sent_after_7_days(self):
         message_count = len(TEST_MESSAGES)
 
-        self.create_email_log_records(1, 6)
+        self.create_email_log_records(1, 6, 'confirm_email')
+        self.create_email_log_records(1, 6, 'complete_registration_prompt')
+        self.create_email_log_records(1, 6, 'dependent_profile_prompt')
 
         # Prompting email should not be sent before 7 days.
-        PromptingEmails().send_set_password_prompts()
+        PromptingEmails().send_confirm_prompting_emails()
+        PromptingEmails().send_complete_registration_prompting_emails()
+        PromptingEmails().send_dependent_profile_prompting_emails()
         self.assertEqual(len(TEST_MESSAGES), message_count)
         db.session.query(EmailLog).delete()
         db.session.commit()
 
-        self.create_email_log_records(1, 8)
+        self.create_email_log_records(1, 8, 'confirm_email')
+        self.create_email_log_records(1, 8, 'complete_registration_prompt')
+        self.create_email_log_records(1, 8, 'dependent_profile_prompt')
 
-        PromptingEmails().send_set_password_prompts()
-        self.assertEqual(len(TEST_MESSAGES), message_count + 1)
-        self.assertEqual("Autism DRIVE: Confirm Email", self.decode(TEST_MESSAGES[-1]['subject']))
+        PromptingEmails().send_confirm_prompting_emails()
+        PromptingEmails().send_complete_registration_prompting_emails()
+        PromptingEmails().send_dependent_profile_prompting_emails()
+        self.assertEqual(len(TEST_MESSAGES), message_count + 3)
+        self.assertEqual("Autism DRIVE: Complete Your Dependent's Profile", self.decode(TEST_MESSAGES[-1]['subject']))
+        self.assertEqual("Autism DRIVE: Complete Your Registration", self.decode(TEST_MESSAGES[-2]['subject']))
+        self.assertEqual("Autism DRIVE: Confirm Email", self.decode(TEST_MESSAGES[-3]['subject']))
 
-    def test_confirm_email_followup_sent_after_14_days(self):
+    def test_prompting_emails_sent_after_14_days(self):
         message_count = len(TEST_MESSAGES)
 
-        self.create_email_log_records(2, 6)
+        self.create_email_log_records(2, 6, 'confirm_email')
+        self.create_email_log_records(2, 6, 'complete_registration_prompt')
+        self.create_email_log_records(2, 6, 'dependent_profile_prompt')
 
         # Prompting email should not be sent between 7 and 14 days.
-        PromptingEmails().send_set_password_prompts()
+        PromptingEmails().send_confirm_prompting_emails()
+        PromptingEmails().send_complete_registration_prompting_emails()
+        PromptingEmails().send_dependent_profile_prompting_emails()
         self.assertEqual(len(TEST_MESSAGES), message_count)
         db.session.query(EmailLog).delete()
         db.session.commit()
 
-        self.create_email_log_records(2, 8)
+        self.create_email_log_records(2, 8, 'confirm_email')
+        self.create_email_log_records(2, 8, 'complete_registration_prompt')
+        self.create_email_log_records(2, 8, 'dependent_profile_prompt')
 
-        PromptingEmails().send_set_password_prompts()
-        self.assertEqual(len(TEST_MESSAGES), message_count + 1)
-        self.assertEqual("Autism DRIVE: Confirm Email", self.decode(TEST_MESSAGES[-1]['subject']))
+        PromptingEmails().send_confirm_prompting_emails()
+        PromptingEmails().send_complete_registration_prompting_emails()
+        PromptingEmails().send_dependent_profile_prompting_emails()
+        self.assertEqual(len(TEST_MESSAGES), message_count + 3)
+        self.assertEqual("Autism DRIVE: Complete Your Dependent's Profile", self.decode(TEST_MESSAGES[-1]['subject']))
+        self.assertEqual("Autism DRIVE: Complete Your Registration", self.decode(TEST_MESSAGES[-2]['subject']))
+        self.assertEqual("Autism DRIVE: Confirm Email", self.decode(TEST_MESSAGES[-3]['subject']))
 
-    def test_confirm_email_followup_sent_after_30_days(self):
+    def test_prompting_emails_sent_after_30_days(self):
         message_count = len(TEST_MESSAGES)
 
-        self.create_email_log_records(3, 12)
+        self.create_email_log_records(3, 12, 'confirm_email')
+        self.create_email_log_records(3, 12, 'complete_registration_prompt')
+        self.create_email_log_records(3, 12, 'dependent_profile_prompt')
 
         # Prompting email should not be sent between 14 and 30 days.
-        PromptingEmails().send_set_password_prompts()
+
+        PromptingEmails().send_confirm_prompting_emails()
+        PromptingEmails().send_complete_registration_prompting_emails()
+        PromptingEmails().send_dependent_profile_prompting_emails()
         self.assertEqual(len(TEST_MESSAGES), message_count)
         db.session.query(EmailLog).delete()
         db.session.commit()
 
-        self.create_email_log_records(3, 16)
+        self.create_email_log_records(3, 16, 'confirm_email')
+        self.create_email_log_records(3, 16, 'complete_registration_prompt')
+        self.create_email_log_records(3, 16, 'dependent_profile_prompt')
 
-        PromptingEmails().send_set_password_prompts()
-        self.assertEqual(len(TEST_MESSAGES), message_count + 1)
-        self.assertEqual("Autism DRIVE: Confirm Email", self.decode(TEST_MESSAGES[-1]['subject']))
+        PromptingEmails().send_confirm_prompting_emails()
+        PromptingEmails().send_complete_registration_prompting_emails()
+        PromptingEmails().send_dependent_profile_prompting_emails()
+        self.assertEqual(len(TEST_MESSAGES), message_count + 3)
+        self.assertEqual("Autism DRIVE: Complete Your Dependent's Profile", self.decode(TEST_MESSAGES[-1]['subject']))
+        self.assertEqual("Autism DRIVE: Complete Your Registration", self.decode(TEST_MESSAGES[-2]['subject']))
+        self.assertEqual("Autism DRIVE: Confirm Email", self.decode(TEST_MESSAGES[-3]['subject']))
 
-    def test_confirm_email_followup_sent_after_60_days(self):
+    def test_prompting_emails_sent_after_60_days(self):
         message_count = len(TEST_MESSAGES)
 
-        self.create_email_log_records(4, 28)
+        self.create_email_log_records(4, 28, 'confirm_email')
+        self.create_email_log_records(4, 28, 'complete_registration_prompt')
+        self.create_email_log_records(4, 28, 'dependent_profile_prompt')
 
         # Prompting email should not be sent between 30 and 60 days.
-        PromptingEmails().send_set_password_prompts()
+
+        PromptingEmails().send_confirm_prompting_emails()
+        PromptingEmails().send_complete_registration_prompting_emails()
+        PromptingEmails().send_dependent_profile_prompting_emails()
         self.assertEqual(len(TEST_MESSAGES), message_count)
         db.session.query(EmailLog).delete()
         db.session.commit()
 
-        self.create_email_log_records(4, 31)
+        self.create_email_log_records(4, 31, 'confirm_email')
+        self.create_email_log_records(4, 31, 'complete_registration_prompt')
+        self.create_email_log_records(4, 31, 'dependent_profile_prompt')
 
-        PromptingEmails().send_set_password_prompts()
-        self.assertEqual(len(TEST_MESSAGES), message_count + 1)
-        self.assertEqual("Autism DRIVE: Confirm Email", self.decode(TEST_MESSAGES[-1]['subject']))
+        PromptingEmails().send_confirm_prompting_emails()
+        PromptingEmails().send_complete_registration_prompting_emails()
+        PromptingEmails().send_dependent_profile_prompting_emails()
+        self.assertEqual(len(TEST_MESSAGES), message_count + 3)
+        self.assertEqual("Autism DRIVE: Complete Your Dependent's Profile", self.decode(TEST_MESSAGES[-1]['subject']))
+        self.assertEqual("Autism DRIVE: Complete Your Registration", self.decode(TEST_MESSAGES[-2]['subject']))
+        self.assertEqual("Autism DRIVE: Confirm Email", self.decode(TEST_MESSAGES[-3]['subject']))
 
-    def test_confirm_email_followup_sent_after_90_days(self):
+    def test_prompting_emails_sent_after_90_days(self):
         message_count = len(TEST_MESSAGES)
 
-        self.create_email_log_records(5, 28)
+        self.create_email_log_records(5, 28, 'confirm_email')
+        self.create_email_log_records(5, 28, 'complete_registration_prompt')
+        self.create_email_log_records(5, 28, 'dependent_profile_prompt')
 
         # Prompting email should not be sent between 60 and 90 days.
-        PromptingEmails().send_set_password_prompts()
+
+        PromptingEmails().send_confirm_prompting_emails()
+        PromptingEmails().send_complete_registration_prompting_emails()
+        PromptingEmails().send_dependent_profile_prompting_emails()
         self.assertEqual(len(TEST_MESSAGES), message_count)
         db.session.query(EmailLog).delete()
         db.session.commit()
 
-        self.create_email_log_records(5, 31)
+        self.create_email_log_records(5, 31, 'confirm_email')
+        self.create_email_log_records(5, 31, 'complete_registration_prompt')
+        self.create_email_log_records(5, 31, 'dependent_profile_prompt')
 
-        PromptingEmails().send_set_password_prompts()
-        self.assertEqual(len(TEST_MESSAGES), message_count + 1)
-        self.assertEqual("Autism DRIVE: Confirm Email", self.decode(TEST_MESSAGES[-1]['subject']))
+        PromptingEmails().send_confirm_prompting_emails()
+        PromptingEmails().send_complete_registration_prompting_emails()
+        PromptingEmails().send_dependent_profile_prompting_emails()
+        self.assertEqual(len(TEST_MESSAGES), message_count + 3)
+        self.assertEqual("Autism DRIVE: Complete Your Dependent's Profile", self.decode(TEST_MESSAGES[-1]['subject']))
+        self.assertEqual("Autism DRIVE: Complete Your Registration", self.decode(TEST_MESSAGES[-2]['subject']))
+        self.assertEqual("Autism DRIVE: Confirm Email", self.decode(TEST_MESSAGES[-3]['subject']))
 
-    def test_confirm_email_does_not_send_more_than_5_times_total(self):
+    def test_prompting_emails_do_not_send_more_than_5_times_total(self):
         message_count = len(TEST_MESSAGES)
 
-        self.create_email_log_records(6, 31)
+        self.create_email_log_records(6, 31, 'confirm_email')
+        self.create_email_log_records(6, 31, 'complete_registration_prompt')
+        self.create_email_log_records(6, 31, 'dependent_profile_prompt')
 
-        PromptingEmails().send_set_password_prompts()
+        PromptingEmails().send_confirm_prompting_emails()
+        PromptingEmails().send_complete_registration_prompting_emails()
+        PromptingEmails().send_dependent_profile_prompting_emails()
         self.assertEqual(len(TEST_MESSAGES), message_count)
