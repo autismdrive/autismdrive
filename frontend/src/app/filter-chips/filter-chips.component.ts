dd5ea131aa6825ec8d41d8df9842bc833f3c1cd3
Eleanor Graham <eleanorcjkgraham@gmail.com>
2020-04-03 13:41:31 -0400

    Fixes #390 - adds COVID category filter chips to resource detail pages

--- frontend/src/app/filter-chips/filter-chips.component.ts
+++ frontend/src/app/filter-chips/filter-chips.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { StudyCategory } from '../_models/study_category';
-import { ResourceCategory } from '../_models/resource_category';
-import {AgeRange, Language} from '../_models/hit_type';
+import {Component, OnInit, Input} from '@angular/core';
+import {StudyCategory} from '../_models/study_category';
+import {ResourceCategory} from '../_models/resource_category';
+import {AgeRange, Language, Covid19Categories} from '../_models/hit_type';
 
 @Component({
   selector: 'app-filter-chips',
@@ -12,12 +12,13 @@ export class FilterChipsComponent implements OnInit {
   @Input() categories: StudyCategory[] | ResourceCategory[] = [];
   @Input() ages: string[] = [];
   @Input() languages: string[] = [];
+  @Input() covid19_categories: string[] = [];
 
   ageLabels = AgeRange.labels;
   languageLabels = Language.labels;
+  covid19Labels = Covid19Categories.labels;
 
   constructor() {
-
   }
 
   ngOnInit() {
