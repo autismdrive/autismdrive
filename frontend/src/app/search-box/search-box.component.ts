0e755d5f1d199600247dd32af136c895f80e42a7
Aaron Louie <ajlouie@gmail.com>
2020-10-13 22:57:01 -0400

    Refines search box autocomplete behavior
    Displays parent breadcrumbs for subtopics in search suggestions. When suggestion is selected, category is selected, rather than using category as keyword search.

--- frontend/src/app/search-box/search-box.component.ts
+++ frontend/src/app/search-box/search-box.component.ts
@@ -3,9 +3,12 @@ import {MatInput} from '@angular/material/input';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 import {Observable, Subject, timer} from 'rxjs';
 import {debounce, debounceTime, distinctUntilChanged, startWith, map} from 'rxjs/operators';
+import {Category} from '../_models/category';
 import {SearchService} from '../_services/api/search.service';
 import {FormControl} from '@angular/forms';
 import {ApiService} from '../_services/api/api.service';
+
+interface CategoryMap { [key: number]: Category; }
 
 @Component({
   selector: 'app-search-box',
@@ -18,10 +21,13 @@ export class SearchBoxComponent implements OnInit, AfterViewInit {
   @Input() words: string;
   @Input() variant: string;
   @Output() searchUpdated = new EventEmitter<Params>();
+  @Output() categorySelected = new EventEmitter<Category>();
   searchUpdate = new Subject<String>();
   searchBoxControl = new FormControl();
-  options: string[] = [];
-  filteredOptions: Observable<string[]>;
+  options: Category[] = [];
+  filteredOptions: Observable<Category[]>;
+  categoryTree: Category[];
+  flattenedCategoryTree: CategoryMap = {};
 
   constructor(
     private route: ActivatedRoute,
@@ -33,14 +39,26 @@ export class SearchBoxComponent implements OnInit, AfterViewInit {
       .queryParams
       .pipe(debounce(() => timer(1000)))
       .subscribe(qp => this.queryParams = qp);
+
     this.searchUpdate.pipe(
       debounceTime(400),
       distinctUntilChanged()
     ).subscribe(value => this.updateSearch(false));
 
-    this.api.getCategoryNamesList().subscribe(categories => {
-      this.options = categories;
+    this.api.getCategoryTree().subscribe(categoryTree => {
+      console.log('category tree', categoryTree);
+      this.categoryTree = categoryTree;
+      const flattened = {};
+      this._flattenCategoryTree(categoryTree, flattened);
+      this.flattenedCategoryTree = this._flattenCategoryTree(categoryTree, flattened);
+      this.options = Object.values(flattened);
+      this._populateCategoryParents();
+      console.log('flattened category tree', this.flattenedCategoryTree);
     });
+
+    // this.api.getCategoryNamesList().subscribe(categories => {
+    //   this.options = categories;
+    // });
   }
 
   @ViewChild('searchInput', {read: MatInput, static: false})
@@ -70,11 +88,14 @@ export class SearchBoxComponent implements OnInit, AfterViewInit {
     });
   }
 
-  private _filter(value: string): string[] {
+  private _filter(value: string): Category[] {
     const filterValue = value.toLowerCase();
-
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+    return this.options.filter(option => {
+      const indentedString = (this.levelIndent(option) + ' ' + option.name);
+      return indentedString.toLowerCase().includes(filterValue);
+    });
   }
+
   updateSearch(removeWords: boolean): Promise<boolean> {
     if (removeWords) {
       this.words = '';
@@ -100,4 +121,41 @@ export class SearchBoxComponent implements OnInit, AfterViewInit {
     return this.searchInputElement && this.searchInputElement.value && (this.searchInputElement.value.length > 0);
   }
 
+  private _flattenCategoryTree(categoryTree: Category[], flattened: CategoryMap) {
+    categoryTree.forEach(c => {
+      if (!flattened[c.id]) { flattened[c.id] = c; }
+
+      if (c.children && c.children.length > 0) {
+        this._flattenCategoryTree(c.children, flattened);
+      }
+    });
+
+    return flattened;
+  }
+
+  private _populateCategoryParents() {
+    this.options.forEach(c => {
+      if (c.parent_id !== null) {
+        c.parent = this.flattenedCategoryTree[c.parent_id];
+        this.flattenedCategoryTree[c.id].parent = c.parent;
+      }
+    });
+  }
+
+  levelIndent(option: Category) {
+    let indentString = '';
+    let parent = option.parent;
+
+    while (parent) {
+      indentString += `${parent.name} > `;
+      parent = parent.parent;
+    }
+
+    return indentString;
+  }
+
+  selectCategory($event) {
+    console.log('$event', $event);
+    this.categorySelected.emit($event.option.value as Category);
+  }
 }
