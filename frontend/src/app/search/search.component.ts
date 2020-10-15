60e24abdf24c3ca3f8bc1b9ec644ee0d6d9c7ad9
Aaron Louie <ajlouie@gmail.com>
2020-10-15 10:00:50 -0400

    Tidies up formatting and code organization

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -1,39 +1,37 @@
-import {AgmMap, LatLngBounds, LatLngLiteral, MapsAPILoader} from '@agm/core';
+import {AgmMap, LatLngBounds, LatLngLiteral} from '@agm/core';
 import {animate, query, stagger, style, transition, trigger} from '@angular/animations';
-import {MediaMatcher} from '@angular/cdk/layout';
 import {Location} from '@angular/common';
 import {
+  AfterViewInit,
   ChangeDetectorRef,
   Component,
+  HostBinding,
   OnDestroy,
   OnInit,
   Renderer2,
-  ViewChild,
-  AfterViewInit,
-  HostBinding
+  ViewChild
 } from '@angular/core';
-import {MatChipList} from '@angular/material/chips';
 import {MatExpansionPanel} from '@angular/material/expansion';
 import {MatPaginator, PageEvent} from '@angular/material/paginator';
 import {MatTabChangeEvent} from '@angular/material/tabs';
+import {Meta} from '@angular/platform-browser';
 import {ActivatedRoute, Params, Router} from '@angular/router';
+import createClone from 'rfdc';
 import {fromEvent} from 'rxjs';
 import {filter, map, pairwise, share, throttleTime} from 'rxjs/operators';
 import {AccordionItem} from '../_models/accordion-item';
 import {Category} from '../_models/category';
 import {AgeRange, HitType, Language} from '../_models/hit_type';
-import {Hit, Query, Sort} from '../_models/query';
+import {Hit, Query} from '../_models/query';
 import {Resource} from '../_models/resource';
 import {Direction} from '../_models/scroll';
 import {SortMethod} from '../_models/sort_method';
+import {Study} from '../_models/study';
 import {User} from '../_models/user';
 import {ApiService} from '../_services/api/api.service';
 import {AuthenticationService} from '../_services/api/authentication-service';
 import {SearchService} from '../_services/api/search.service';
 import {GoogleAnalyticsService} from '../google-analytics.service';
-import {Meta} from '@angular/platform-browser';
-import {Study} from '../_models/study';
-import createClone from 'rfdc';
 
 declare var google: any;
 
@@ -52,7 +50,7 @@ class MapControlDiv extends HTMLDivElement {
         query('#age-filter, #language-filter, #topic-filter', [
           style({opacity: 0, transform: 'translateX(-100px)'}),
           stagger(-30, [
-            animate('500ms cubic-bezier(0.35, 0, 0.25, 1)', style({ opacity: 1, transform: 'none' }))
+            animate('500ms cubic-bezier(0.35, 0, 0.25, 1)', style({opacity: 1, transform: 'none'}))
           ])
         ])
       ])
@@ -186,9 +184,6 @@ export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
   showFilters: boolean;
   expandResults: boolean;
   restrictToMappedResults: boolean;
-  private mapBounds: LatLngBounds;
-  private scrollDirection: Direction;
-
   searchBgClasses = [
     'light-gray',
     'white',
@@ -215,6 +210,8 @@ and the
 [Frank Porter Graham Child Development Institute](https://afirm.fpg.unc.edu/selecting-ebp).
 `;
   expandTheme = false;
+  private mapBounds: LatLngBounds;
+  private scrollDirection: Direction;
 
   constructor(
     private changeDetectorRef: ChangeDetectorRef,
@@ -233,13 +230,13 @@ and the
     this.ageOptions = this.getOptions(AgeRange.labels);
 
     this.meta.updateTag(
-        { property: 'og:image', content: window.location.origin + '/assets/home/hero-parent-child.jpg' },
-        `property='og:image'`);
+      {property: 'og:image', content: window.location.origin + '/assets/home/hero-parent-child.jpg'},
+      `property='og:image'`);
     this.meta.updateTag(
-      { property: 'og:image:secure_url', content: window.location.origin + '/assets/home/hero-parent-child.jpg' },
+      {property: 'og:image:secure_url', content: window.location.origin + '/assets/home/hero-parent-child.jpg'},
       `property='og:image:secure_url'`);
     this.meta.updateTag(
-      { name: 'twitter:image', content: window.location.origin + '/assets/home/hero-parent-child.jpg' },
+      {name: 'twitter:image', content: window.location.origin + '/assets/home/hero-parent-child.jpg'},
       `name='twitter:image'`);
   }
 
@@ -261,6 +258,53 @@ and the
 
   get showLocationForm(): boolean {
     return !this.noLocation && !this.setLocOpen;
+  }
+
+  get shouldShowMap() {
+    const is_location_or_event_type = this.mapQuery &&
+      this.mapQuery.types &&
+      this.mapQuery.types.length === 1 &&
+      (this.mapQuery.types.includes('location') ||
+        this.mapQuery.types.includes('event'));
+    const is_sort_by_distance = this.selectedSort.name === 'Distance';
+    return is_location_or_event_type || is_sort_by_distance;
+  }
+
+  get hits(): Hit[] {
+    if (this.query && this.query.hits && this.query.hits.length > 0) {
+      if (this.restrictToMappedResults) {
+        return this.mapQuery.hits.filter(h => {
+          if (h.hasCoords()) {
+            const latLng = new google.maps.LatLng(h.latitude, h.longitude);
+            return (this.mapBounds && this.mapBounds.contains(latLng));
+          }
+        });
+      } else {
+        return this.query.hits;
+      }
+    }
+
+    return [];
+  }
+
+  get isLastPage(): boolean {
+    return (this.query.start + this.pageSize) > this.query.total;
+  }
+
+  get numResultsFrom(): number {
+    return (this.paginatorElement.pageIndex * this.pageSize) + 1;
+  }
+
+  get numResultsTo(): number {
+    return this.isLastPage ? this.query.total : (this.paginatorElement.pageIndex + 1) * this.pageSize;
+  }
+
+  get numTotalResults() {
+    return this.query.total;
+  }
+
+  get shouldHideVideo() {
+    return !!localStorage.getItem('shouldHideVideo');
   }
 
   ngOnInit() {
@@ -357,7 +401,7 @@ and the
         this.api.getStudiesByStatus('currently_enrolling').subscribe(studies => {
           this.highlightedStudy = studies[Math.floor(Math.random() * Math.floor(studies.length))];
           this.changeDetectorRef.detectChanges();
-      });
+        });
       }
     });
     this.googleAnalyticsService.searchEvent(this.query);
@@ -574,16 +618,6 @@ and the
     this.changeDetectorRef.detectChanges();
   }
 
-  get shouldShowMap() {
-    const is_location_or_event_type = this.mapQuery &&
-      this.mapQuery.types &&
-      this.mapQuery.types.length === 1 &&
-      (this.mapQuery.types.includes('location') ||
-        this.mapQuery.types.includes('event'));
-    const is_sort_by_distance = this.selectedSort.name === 'Distance';
-    return is_location_or_event_type || is_sort_by_distance;
-  }
-
   openSetLocation() {
     this.setLocOpen = true;
   }
@@ -592,7 +626,7 @@ and the
     this.setLocOpen = false;
   }
 
-  zipSubmit($event: MouseEvent|KeyboardEvent, setLocationExpansionPanel: MatExpansionPanel): void {
+  zipSubmit($event: MouseEvent | KeyboardEvent, setLocationExpansionPanel: MatExpansionPanel): void {
     setLocationExpansionPanel.close();
     $event.stopPropagation();
     localStorage.setItem('zipCode', this.updatedZip || '');
@@ -601,7 +635,7 @@ and the
     this.reSort('Distance', true);
   }
 
-  useGPSLocation($event: MouseEvent|KeyboardEvent, setLocationExpansionPanel: MatExpansionPanel): void {
+  useGPSLocation($event: MouseEvent | KeyboardEvent, setLocationExpansionPanel: MatExpansionPanel): void {
     setLocationExpansionPanel.close();
     $event.stopPropagation();
     localStorage.removeItem('zipCode');
@@ -632,7 +666,10 @@ and the
     return this.selectedMapResource != null;
   }
 
-  // https://stackoverflow.com/a/19303725/1791917
+  /**
+   * Returns a random number for the given seed
+   * https://stackoverflow.com/a/19303725/1791917
+   **/
   mapJitter(seed: number, isLat: boolean): number {
     let m = seed % 2 === 0 ? 1 : -1;
     if (isLat) {
@@ -659,104 +696,6 @@ and the
     return {url: url, anchor: {x: x, y: y}};
   }
 
-  protected mapLoad(m: google.maps.Map) {
-    this.addMyLocationControl(m);
-  }
-
-  private _queryToQueryParams(q: Query): Params {
-    const queryParams: Params = {};
-
-    if (q.hasOwnProperty('words') && q.words) {
-      queryParams.words = q.words;
-    } else {
-      queryParams.words = undefined;
-    }
-
-    queryParams.types = q.types;
-    queryParams.ages = q.ages;
-    queryParams.languages = q.languages;
-    queryParams.sort = this.selectedSort.name;
-    queryParams.pageStart = q.start;
-
-    if (q.hasOwnProperty('category') && q.category) {
-      queryParams.category = q.category.id;
-    }
-    return queryParams;
-  }
-
-  get hits(): Hit[] {
-    if (this.query && this.query.hits && this.query.hits.length > 0) {
-      if (this.restrictToMappedResults) {
-        return this.mapQuery.hits.filter(h => {
-          if (h.hasCoords()) {
-            const latLng = new google.maps.LatLng(h.latitude, h.longitude);
-            return (this.mapBounds && this.mapBounds.contains(latLng));
-          }
-        });
-      } else {
-        return this.query.hits;
-      }
-    }
-
-    return [];
-  }
-
-  // Return a random number for the given seed
-  private _queryParamsToQuery(qParams: Params): Query {
-
-    const q = new Query({});
-    q.size = this.pageSize;
-    if (qParams && qParams.keys) {
-      for (const key of qParams.keys) {
-        switch (key) {
-          case ('words'):
-            q.words = qParams.get(key);
-            break;
-          case ('category'):
-            q.category = {'id': qParams.get(key)};
-            break;
-          case('ages'):
-            q.ages = qParams.getAll(key);
-            break;
-          case('languages'):
-            q.languages = qParams.getAll(key);
-            break;
-          case('sort'):
-            if (this.sortMethods.find(m => m.name === qParams.get(key)) !== undefined) {
-              q.sort = this.sortMethods.find(m => m.name === qParams.get(key)).sortQuery;
-            }
-            break;
-          case('pageStart'):
-            this.selectedPageStart = Number(qParams.get(key));
-            break;
-          case('types'):
-            q.types = qParams.getAll(key);
-        }
-      }
-    }
-    return q;
-
-  }
-
-  private _updateDistanceSort() {
-    const distance_sort = this.sortMethods.find(s => s.name === 'Distance');
-    distance_sort.sortQuery.latitude = this.noLocation ? this.defaultLoc.lat : this.mapLoc.lat;
-    distance_sort.sortQuery.longitude = this.noLocation ? this.defaultLoc.lng : this.mapLoc.lng;
-    if (this.selectedSort.name === 'Distance') {
-      this.selectedSort = distance_sort;
-      this.query.sort = this.selectedSort.sortQuery;
-      this.updateUrlAndDoSearch();
-    }
-  }
-
-  private _goToFirstPage() {
-    this.query.start = 0;
-    if (this.paginatorElement) {
-      this.paginatorElement.firstPage();
-    }
-    this.updateUrlAndDoSearch();
-  }
-
   selectTypeTab($event: MatTabChangeEvent) {
     const resourceType = ($event.index > 0) ? this.resourceTypesFiltered()[$event.index - 1] : HitType.ALL_RESOURCES;
     this.selectType(resourceType.name);
@@ -772,22 +711,6 @@ and the
     if (shouldRestrict) {
       this.googleAnalyticsService.searchInteractionEvent('search_as_map_moves');
     }
-  }
-
-  isLastPage(): boolean {
-    return (this.query.start + this.pageSize) > this.query.total;
-  }
-
-  numResultsFrom(): number {
-    return (this.paginatorElement.pageIndex * this.pageSize) + 1;
-  }
-
-  numResultsTo(): number {
-    return this.isLastPage() ? this.query.total : (this.paginatorElement.pageIndex + 1) * this.pageSize;
-  }
-
-  numTotalResults() {
-    return this.query.total;
   }
 
   mapDockClass(scrollSpy: HTMLSpanElement, searchHeader: HTMLDivElement, searchFooter: HTMLDivElement): string {
@@ -807,14 +730,6 @@ and the
     }
 
     return alignClass + ' ' + scrollDirection;
-  }
-
-  private _overlaps(a: ClientRect | DOMRect, b: ClientRect | DOMRect): boolean {
-    return (
-      ((b.top < a.top) && (b.bottom > a.top)) ||      // b overlaps top edge of a
-      ((b.top > a.top) && (b.bottom < a.bottom)) ||   // b inside a
-      ((b.top < a.bottom) && (b.bottom > a.bottom))   // b overlaps bottom edge of a
-    );
   }
 
   focusOnInput(zipCodeInput: HTMLInputElement) {
@@ -874,12 +789,95 @@ and the
     }
   }
 
-  get shouldHideVideo() {
-    return !!localStorage.getItem('shouldHideVideo');
-  }
-
   showVideo(className: string) {
     this.videoPlacement = className;
     this.hideVideo(false);
+  }
+
+  protected mapLoad(m: google.maps.Map) {
+    this.addMyLocationControl(m);
+  }
+
+  private _queryToQueryParams(q: Query): Params {
+    const queryParams: Params = {};
+
+    if (q.hasOwnProperty('words') && q.words) {
+      queryParams.words = q.words;
+    } else {
+      queryParams.words = undefined;
+    }
+
+    queryParams.types = q.types;
+    queryParams.ages = q.ages;
+    queryParams.languages = q.languages;
+    queryParams.sort = this.selectedSort.name;
+    queryParams.pageStart = q.start;
+
+    if (q.hasOwnProperty('category') && q.category) {
+      queryParams.category = q.category.id;
+    }
+    return queryParams;
+  }
+
+  private _queryParamsToQuery(qParams: Params): Query {
+    const q = new Query({});
+    q.size = this.pageSize;
+    if (qParams && qParams.keys) {
+      for (const key of qParams.keys) {
+        switch (key) {
+          case ('words'):
+            q.words = qParams.get(key);
+            break;
+          case ('category'):
+            q.category = {'id': qParams.get(key)};
+            break;
+          case('ages'):
+            q.ages = qParams.getAll(key);
+            break;
+          case('languages'):
+            q.languages = qParams.getAll(key);
+            break;
+          case('sort'):
+            if (this.sortMethods.find(m => m.name === qParams.get(key)) !== undefined) {
+              q.sort = this.sortMethods.find(m => m.name === qParams.get(key)).sortQuery;
+            }
+            break;
+          case('pageStart'):
+            this.selectedPageStart = Number(qParams.get(key));
+            break;
+          case('types'):
+            q.types = qParams.getAll(key);
+        }
+      }
+    }
+    return q;
+
+  }
+
+  private _updateDistanceSort() {
+    const distance_sort = this.sortMethods.find(s => s.name === 'Distance');
+    distance_sort.sortQuery.latitude = this.noLocation ? this.defaultLoc.lat : this.mapLoc.lat;
+    distance_sort.sortQuery.longitude = this.noLocation ? this.defaultLoc.lng : this.mapLoc.lng;
+    if (this.selectedSort.name === 'Distance') {
+      this.selectedSort = distance_sort;
+      this.query.sort = this.selectedSort.sortQuery;
+      this.updateUrlAndDoSearch();
+    }
+  }
+
+  private _goToFirstPage() {
+    this.query.start = 0;
+    if (this.paginatorElement) {
+      this.paginatorElement.firstPage();
+    }
+    this.updateUrlAndDoSearch();
+  }
+
+  private _overlaps(a: ClientRect | DOMRect, b: ClientRect | DOMRect): boolean {
+    return (
+      ((b.top < a.top) && (b.bottom > a.top)) ||      // b overlaps top edge of a
+      ((b.top > a.top) && (b.bottom < a.bottom)) ||   // b inside a
+      ((b.top < a.bottom) && (b.bottom > a.bottom))   // b overlaps bottom edge of a
+    );
   }
 }
