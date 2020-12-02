c688e044a90a9368b8c1351e43dd024cac7152ed
Elad <elad@yaromglobal.com>
2020-12-02 13:12:29 -0400

    committing dockerization progress, much cleaner now

--- backend/containerrun.sh
+++ backend/containerrun.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
-sleep 30
+timeout 300 bash -c "until curl --silent --output /dev/null http://star-drive_es_1:9200/_cat/health?h=st; do printf '.'; sleep 5; done; printf '\n'"
 flask db init
 sleep 5
 flask db migrate
 sleep 5
 flask db upgrade
 sleep 5
-flask initdb
-sleep 5
+psql $(grep -i "SQLALCHEMY_DATABASE_URI" /star-drive/backend/instance/config.py | awk -F'"' '{print $2}') -lqt | cut -d \| -f 1 | grep stardrive | grep 0 && flask initdb
 flask run --host 0.0.0.0
