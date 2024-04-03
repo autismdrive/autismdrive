153bccc04c8796016794bc0996be676565a8ddfb
Aaron Louie <ajlouie@gmail.com>
2024-04-03 16:11:25 -0400

    Fixes breaking changed caused by last JWT package update.

--- backend/app/resources/Auth.py
+++ backend/app/resources/Auth.py
@@ -12,7 +12,7 @@ from flask import g, request, Blueprint, jsonify
 
 from app.schema.schema import UserSchema
 
-auth_blueprint = Blueprint('auth', __name__, url_prefix='/api')
+auth_blueprint = Blueprint("auth", __name__, url_prefix="/api")
 
 
 def confirm_email(email_token):
@@ -29,19 +29,19 @@ def confirm_email(email_token):
     db.session.add(user)
     db.session.commit()
 
-    auth_token = user.encode_auth_token().decode()
+    auth_token = user.encode_auth_token()
     user.token = auth_token
     return user
 
 
-@auth_blueprint.route('/login_password', methods=["GET", "POST"])
+@auth_blueprint.route("/login_password", methods=["GET", "POST"])
 def login_password():
     request_data = request.get_json()
 
     if request_data is None:
         raise RestException(RestException.INVALID_INPUT)
 
-    email = request_data['email']
+    email = request_data["email"]
     user = session.query(User).filter(func.lower(User.email) == email.lower()).first()
     schema = UserSchema(many=False)
 
@@ -50,7 +50,7 @@ def login_password():
     if user.email_verified:
         if user.is_correct_password(request_data["password"]):
             # redirect users back to the front end, include the new auth token.
-            auth_token = user.encode_auth_token().decode()
+            auth_token = user.encode_auth_token()
             g.user = user
             user.token = auth_token
             user.last_login = datetime.datetime.utcnow()
@@ -60,17 +60,17 @@ def login_password():
         else:
             raise RestException(RestException.LOGIN_FAILURE)
     else:
-        if 'email_token' in request_data:
-            g.user = confirm_email(request_data['email_token'])
+        if "email_token" in request_data:
+            g.user = confirm_email(request_data["email_token"])
             return schema.jsonify(user)
         else:
             raise RestException(RestException.CONFIRM_EMAIL)
 
 
-@auth_blueprint.route('/forgot_password', methods=["GET", "POST"])
+@auth_blueprint.route("/forgot_password", methods=["GET", "POST"])
 def forgot_password():
     request_data = request.get_json()
-    email = request_data['email']
+    email = request_data["email"]
     user = session.query(User).filter(func.lower(User.email) == func.lower(email)).first()
 
     if user:
@@ -80,35 +80,35 @@ def forgot_password():
         db.session.add(log)
         db.session.commit()
 
-        if (app.config.get('TESTING') or app.config.get('DEVELOPMENT')) and user.token_url:
+        if (app.config.get("TESTING") or app.config.get("DEVELOPMENT")) and user.token_url:
             return jsonify(user.token_url)
         else:
-            return ''
+            return ""
     else:
         raise RestException(RestException.EMAIL_NOT_REGISTERED)
 
 
-@auth_blueprint.route('/reset_password', methods=["GET", "POST"])
+@auth_blueprint.route("/reset_password", methods=["GET", "POST"])
 def reset_password():
     request_data = request.get_json()
-    password = request_data['password']
-    email_token = request_data['email_token']
+    password = request_data["password"]
+    email_token = request_data["email_token"]
     try:
         ts = URLSafeTimedSerializer(app.config["SECRET_KEY"])
-        email = ts.loads(email_token, salt="email-reset-key", max_age=86400) #24 hours
+        email = ts.loads(email_token, salt="email-reset-key", max_age=86400)  # 24 hours
     except SignatureExpired:
         raise RestException(RestException.TOKEN_EXPIRED)
     except BadSignature:
         raise RestException(RestException.TOKEN_INVALID)
 
     user = session.query(User).filter(func.lower(User.email) == email.lower()).first_or_404()
-    user.token_url = ''
+    user.token_url = ""
     user.email_verified = True
     user.password = password
     user.last_login = datetime.datetime.utcnow()
     db.session.add(user)
     db.session.commit()
-    auth_token = user.encode_auth_token().decode()
+    auth_token = user.encode_auth_token()
     user.token = auth_token
     return UserSchema().jsonify(user)
 
@@ -122,8 +122,8 @@ def verify_token(token):
     except:
         g.user = None
 
-    if 'user' in g and g.user:
-        g.user.token_url = ''
+    if "user" in g and g.user:
+        g.user.token_url = ""
         return True
     else:
         return False
@@ -132,9 +132,9 @@ def verify_token(token):
 def login_optional(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        if request.method != 'OPTIONS':  # pragma: no cover
+        if request.method != "OPTIONS":  # pragma: no cover
             try:
-                auth = verify_token(request.headers['AUTHORIZATION'].split(' ')[1])
+                auth = verify_token(request.headers["AUTHORIZATION"].split(" ")[1])
             except:
                 auth = False
 
