a5989dc3fa8cf6386882f70c7e8948e77749a6c6
Aaron Louie <ajlouie@gmail.com>
2024-11-20 14:57:11 -0500

    Fixes testing environment variable

--- backend/config/ci.py
+++ backend/config/ci.py
@@ -2,7 +2,7 @@ from config.base import Settings
 
 settings = Settings(
     SQLALCHEMY_DATABASE_URI="postgresql+psycopg://ed_user:ed_pass@localhost/stardrive",
-    TESTING=False,
+    TESTING=True,
     CORS_ENABLED=True,
     DEBUG=True,
     DEVELOPMENT=True,
