6c021c53481993de3911f4c6dfb9e2119431cb1a
Aaron Louie <ajlouie@gmail.com>
2024-12-19 10:57:38 -0500

    Instantiates app here rather than importing instance

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,4 @@
-from app.main import app
+from app.create_app import create_app
 import dotenv
 import os
 
@@ -8,5 +8,6 @@ dotenv.load_dotenv(dotenv_path=".env")
 port = int(os.environ.get("FLASK_RUN_PORT", 5000))
 
 print("Running on port " + str(port))
+app = create_app()
 app.run(host="0.0.0.0", threaded=True, port=port)
 app.schedule_tasks()
