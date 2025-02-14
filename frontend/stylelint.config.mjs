2250a713afccd906584d0ac047d37a6edd28a96a
Aaron Louie <ajlouie@gmail.com>
2025-02-14 12:21:12 -0500

    Fixes SCSS problems identified by stylelint

--- frontend/stylelint.config.mjs
+++ frontend/stylelint.config.mjs
@@ -3,6 +3,7 @@ export default {
   extends: ['stylelint-config-standard-scss'],
   rules: {
     'selector-attribute-quotes': 'always',
-    'no-empty-source': false,
+    'no-empty-source': null,
+    'no-descending-specificity': null,
   },
 };
