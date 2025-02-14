85c429eda89ef91c7ae9e8a0a1fdd40e378b1538
Aaron Louie <ajlouie@gmail.com>
2025-02-14 11:50:14 -0500

    WIP: Fixes some problems identified by stylelint.

--- frontend/stylelint.config.mjs
+++ frontend/stylelint.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ['stylelint-config-standard-scss'],
+  rules: {
+    'selector-attribute-quotes': 'always',
+    'no-empty-source': false,
+  },
 };
